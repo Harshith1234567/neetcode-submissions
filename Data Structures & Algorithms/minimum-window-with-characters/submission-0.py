b2fc5c1c1d19c2cea19a16e857c34693df9d4a76
Harshith S R <53342028+Harshith1234567@users.noreply.github.com>
2026-09-19 11:17:47 -0700

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,58 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        sdicto=defaultdict(int)
+        tdicto={}
+
+        for a in t:
+            tdicto[a]=1+tdicto.get(a,0)
+
+        # print(tdicto)
+        
+        m=26-len(tdicto)
+        if m==26:
+            return True
+
+        l=0
+        res=s
+        flag=0
+        for r,b in enumerate(s):
+            sdicto[b]+=1
+            # print(sdicto)
+
+            if b in tdicto and sdicto[b]==tdicto[b] and sdicto[b]-1<tdicto[b]:
+                m+=1
+
+            # print(r,b, m)
+
+            
+
+            while m==26:
+                flag=1
+                if len(res) > (r-l+1):
+                    res=s[l:r+1]
+
+                
+
+                
+                a=s[l]
+                
+                if a not in tdicto or sdicto[a] > tdicto[a]:
+                    sdicto[s[l]]-=1
+                    l+=1
+                else:
+
+                    break
+
+        return res if flag else ''
+
+
+
+
+
+            
+
+            
+
+
+        
